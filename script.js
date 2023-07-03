70893ce3c7831e5c6f68b7f2e1f1bfa228a76b79
AlexProou <121025618+AlexProou@users.noreply.github.com>
2023-07-03 14:50:31 +0300

    style_8

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.querySelector('#menu-btn').onclick = () =>{
 
 $("#block").fadeIn(1000);
 $("#block_p").fadeIn(2000);
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
