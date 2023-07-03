a68e8c31900c2c11ca38a3d4f7b2fda167d209eb
AlexProou <121025618+AlexProou@users.noreply.github.com>
2023-07-03 16:29:58 +0300

    style_9

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ document.querySelector('#menu-btn').onclick = () =>{
 $("#block").fadeIn(1000);
 $("#block_p").fadeIn(2000);
 
+$("#draw_circle1").fadeIn(2200);
+$("#draw_circle2").fadeIn(2400);
+$("#draw_circle3").fadeIn(2600);
+$("#draw_circle4").fadeIn(2800);
+$("#draw_circle5").fadeIn(3000);
 
-function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('element-show');
-      }
-    });
-  }
-  let options = { threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
+
+//function onEntry(entry) {
+//    entry.forEach(change => {
+//      if (change.isIntersecting) {
+//        change.target.classList.add('box-container');
+//      }
+ //   });
+ // }
+//  let options = { threshold: [0.5] };
+///  let observer = new IntersectionObserver(onEntry, options);
+//  let elements = document.querySelectorAll('box-container');
+//  for (let elm of elements) {
+ //   observer.observe(elm);
+ // }
